0cc3b395413bda1913ef553ef3ea004441fcdf21
Zikani Nyirenda Mwase <zikani.nmwase@ymail.com>
2023-01-21 04:29:55 +0200

    chore: move autoload to main file, add server_address config

--- databaseconnection/connection.go
+++ databaseconnection/connection.go
@@ -7,12 +7,10 @@ import (
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/joho/godotenv"
 )
 
 func Connect() sql.DB {
-	godotenv.Load(".env")
-	//localhost := os.Getenv("localhost")
+
 	password := os.Getenv("password")
 	user := os.Getenv("user")
 	database := os.Getenv("database")
--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/preciousnyasulu/malawi-country-data/routes"
 )
 
 func main() {
+	godotenv.Load(".env")
+	//localhost := os.Getenv("localhost")
 	router := gin.Default()
 	//District routes
 	router.GET("/District", routes.GetDistricts)
@@ -18,5 +24,10 @@ func main() {
 
 	//Village routes
 	router.GET("/Village", routes.GetVillages)
-	router.Run("localhost:8000")
+
+	address := os.Getenv("server_address")
+	err := router.Run(address)
+	if err != nil {
+		log.Fatalf("failed to start server at %s got error %v", address, err)
+	}
 }
