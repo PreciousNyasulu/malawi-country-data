1b8b08133c4ef8b0c26406d6ad10c10faf320fe7
preciousnyasulu <preciousnyasulu441@gmail.com>
2023-02-11 12:04:53 +0200

    Refactor connection string to Azure Mysql

--- databaseconnection/connection.go
+++ databaseconnection/connection.go
@@ -6,23 +6,22 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func Connect() *sql.DB {
 	password := os.Getenv("password")
-	host := os.Getenv("host")
+	host := os.Getenv("server")
 	user := os.Getenv("user")
 	database := os.Getenv("database")
 
-	port, err := strconv.Atoi(os.Getenv("port"))
-	if err != nil {
-		port = 3306
-	}
+	// port, err := strconv.Atoi(os.Getenv("port"))
+	// if err != nil {
+	// 	port = 3306
+	// }
 
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", user, password, host, port, database))
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?allowNativePasswords=true", user, password, host, database))
 	if err != nil {
 		log.Fatal("Error creating connection pool: ", err.Error())
 	}
