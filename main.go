e01d54ad3db5df925818bf7138909cd84e92eb32
PreciousNyasulu <preciousnyasulu441@gmail.com>
2023-06-08 23:12:31 +0200

    [feat]- all districts route

--- main.go
+++ main.go
@@ -2,16 +2,29 @@ package main
 
 import (
 	"log"
+	"malawi-country-data/src/routes"
+	_"malawi-country-data/docs"
+	"net/http"
 	"os"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	"malawi-country-data/src/routes"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	swaggerFiles "github.com/swaggo/files"
 )
 
+// @title Malawi Country Data API
+// @version 1.0
+// @description An API for the malawian country details 
+// @host localhost:8080
+// @Basepath /api
 func main() {
 	godotenv.Load()
 	//localhost := os.Getenv("localhost")
 	router := gin.Default()
+
+	// Documentation
+	router.GET("/docs/*any",ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 	//District routes
 	router.GET("/api/District", routes.GetDistricts)
 	router.GET("/api/District/Region/:region", routes.GetDistrictByRegion)
@@ -38,8 +51,15 @@ func main() {
 	router.GET("/api/Wards/Region/:search", routes.SearchWardWithRegion)
 	router.GET("/api/Wards/District/:search", routes.SearchWardWithDistrict)
 
+	// Healthcheck endpoint
+	router.GET("/HealthCheck", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"healthy": true,
+		})
+	})
+
 	address := os.Getenv("server_address")
-	err := router.Run(address)
+	err := router.Run(":8080")
 	if err != nil {
 		log.Fatalf("failed to start server at %s got error %v", address, err)
 	}
